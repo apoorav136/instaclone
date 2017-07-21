723e392026cbf2969a95704617ff51e12ddec1f5
Apoorav sharma <apooravsharma136@gmail.com>
2017-07-21 22:28:38 +0530

    Add files via upload
    some validations added and error message displayed.

--- myapp/views.py
+++ myapp/views.py
@@ -9,7 +9,10 @@ import sendgrid
 from api import SENDGRID_API_KEY
 from sendgrid.helpers.mail import *
 from imgurpython import ImgurClient
-
+from django.core.mail import send_mail
+from paralleldots import set_api_key, sentiment
+from api import  YOUR_API_KEY
+import ctypes
 YOUR_CLIENT_ID = '0002161fe35de3d'
 YOUR_CLIENT_SECRET = "f45b827e48c1444021046778a2c3e3e573432709"
 
@@ -22,20 +25,27 @@ def signup_view(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            # saving data to DB
-            user = UserModel(name=name, password=make_password(password), email=email, username=username)
-            user.save()
-            sg = sendgrid.SendGridAPIClient(apikey=(SENDGRID_API_KEY))
-            from_email = Email("apooravsharma1997@gamil.com")
-            to_email = Email(form.cleaned_data['email'])
-            subject = "Welcome to Review book??"
-            content = Content("text/plain", "Thank you for signing up  with REVIEW BOOK. /n We provide best reviews on various products which makes easy choices for you./n Team , REVIEW BOOK.""  ??????")
-            mail = Mail(from_email, subject, to_email, content)
-            response = sg.client.mail.send.post(request_body=mail.get())
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-            return render(request, 'login.html')
+
+
+            if len(username)>4 and len(password)>5 :
+                user = UserModel(name=name, password=make_password(password), email=email, username=username)
+                user.save()
+                sg = sendgrid.SendGridAPIClient(apikey=(SENDGRID_API_KEY))
+                from_email = Email("apooravsharma1997@gamil.com")
+                to_email = Email(form.cleaned_data['email'])
+                subject = "Welcome to Review book??"
+                content = Content("text/plain", "Thank you for signing up  with REVIEW BOOK. /n We provide best reviews on various products which makes easy choices for you./n Team , REVIEW BOOK.""  ??????")
+                mail = Mail(from_email, subject, to_email, content)
+                response = sg.client.mail.send.post(request_body=mail.get())
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+                ctypes.windll.user32.MessageBoxW(0, u"successfully signed up", u"success", 0)
+                return render(request, 'login.html')
+
+            else:
+                ctypes.windll.user32.MessageBoxW(0, u"invalid enteries. please try again", u"Error", 0)
+                form= SignUpForm()
 
     else:
         form = SignUpForm()
@@ -61,8 +71,10 @@ def login_view(request):
                     response.set_cookie(key='session_token', value=token.session_token)
                     return response
                 else:
+                    ctypes.windll.user32.MessageBoxW(0, u"invalid username or password", u"Error", 0)
                     response_data['message'] = 'Incorrect Password! Please try again!'
-
+            else:
+                ctypes.windll.user32.MessageBoxW(0, u"invalid username or password", u"Error", 0)
     elif request.method == 'GET':
         form = LoginForm()
 
@@ -86,9 +98,11 @@ def post_view(request):
                 client = ImgurClient(YOUR_CLIENT_ID, YOUR_CLIENT_SECRET)
                 post.image_url = client.upload_from_path(path, anon=True)['link']
                 post.save()
+                ctypes.windll.user32.MessageBoxW(0, u"post successsfully created", u"SUCCESS", 0)
                 return redirect('/feed/')
 
         else:
+
             form = PostForm()
         return render(request, 'post.html', {'form': form})
     else:
@@ -108,14 +122,13 @@ def check_validation(request):
 def feed_view(request):
     user = check_validation(request)
     if user:
-
         posts = PostModel.objects.all().order_by('created_on')
-
         for post in posts:
             existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
+
+
             if existing_like:
                 post.has_liked = True
-
         return render(request, 'feed.html', {'posts': posts})
     else:
 
@@ -138,7 +151,6 @@ def like_view(request):
     else:
         return redirect('/login/')
 
-
 def comment_view(request):
     user = check_validation(request)
     if user and request.method == 'POST':
@@ -148,6 +160,7 @@ def comment_view(request):
             comment_text = form.cleaned_data.get('comment_text')
             comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
             comment.save()
+
             return redirect('/feed/')
         else:
             return redirect('/feed/')
@@ -165,3 +178,4 @@ def check_validation(request):
                 return session.user
     else:
         return None
+
