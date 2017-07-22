d14cbfa8da0e461e80fe8c356cd79b5de7c79b4d
Apoorav sharma <apooravsharma136@gmail.com>
2017-07-22 12:01:22 +0530

    url of logout added in this
    url of logout added in this

--- instagramclone/urls.py
+++ instagramclone/urls.py
@@ -6,13 +6,14 @@ from django.conf.urls import url
 #admin panel is created which has a major control overdatabase
 from django.contrib import admin
 #from views we import pages which we are giving url to
-from myapp.views import signup_view, login_view, post_view, feed_view, like_view, comment_view
+from myapp.views import signup_view, login_view, post_view, feed_view, like_view, comment_view,logout_view
 
 # there are the pattenrs which we use to create url for a particular page
 # r is the regular expression
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('logout/',logout_view,name='logout'),
     url('post/', post_view),
     url('feed/', feed_view),
     url('like/', like_view),
