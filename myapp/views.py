c4a0365b0923349372d7eda4364e48c6d9ccfc85
Apoorav sharma <apooravsharma136@gmail.com>
2017-07-26 01:13:59 +0530

    post with latest timestamp to be displayed first

--- myapp/views.py
+++ myapp/views.py
@@ -136,7 +136,7 @@ def check_validation(request):
 def feed_view(request):
     user = check_validation(request)
     if user:
-        posts = PostModel.objects.all().order_by('created_on')
+        posts = PostModel.objects.all().order_by('-created_on')
         for post in posts:
             existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
             if existing_like:
